0179463ef1b0a9be61285d420de7e001aaee3169
Dan-Lupu <danutslupu20@gmail.com>
2023-05-27 22:11:42 +0200

    TurtleCheck

--- RocketLeagueDLL/Hack.h
+++ RocketLeagueDLL/Hack.h
@@ -1,15 +1,41 @@
 #pragma once
 #include "pch.h"
+#include "Offsets.h"
 #include "mem.h"
 #include "proc.h"
 
 void Run()
 {
-	static uintptr_t moduleBaseAddr = (uintptr_t)GetModuleHandle(L"RocketLeague.exe") + 0x022E36F8;
-	static std::vector<unsigned int> Offsets = { 0x930, 0x50, 0x20, 0x218, 0xF0, 0x480, 0x98};
-	static int* BoostAmount = (int*)proc::FindDMAAddyInternal(moduleBaseAddr, Offsets);
+	static bool Worked = false;
+	static uintptr_t moduleBaseAddr = (uintptr_t)GetModuleHandle(L"RocketLeague.exe") + 0x02525040;
+	static std::vector<unsigned int> BoostOffsets = { 0x58, 0xAC0, 0x8, 0xB0, 0x340, 0x60, 0x98 };
+	static std::vector<unsigned int> TurtlingOffsets = { 0x58, 0xAC0, 0x8, 0xB0, 0x340, 0x60, 0x9C };
+	static int* BoostAmount = (int*)proc::FindDMAAddyInternal(moduleBaseAddr, BoostOffsets);
+	static int* Turtling = (int*)proc::FindDMAAddyInternal(moduleBaseAddr, TurtlingOffsets);
 	if (GetAsyncKeyState(VK_SPACE))
 	{
-		std::cout <<  *BoostAmount << std::dec << std::endl;
+		Worked = false;
+		if (BoostAmount != NULL && *BoostAmount != NULL && *BoostAmount <= 100 && *BoostAmount >= 0)
+		{
+			std::cout << *BoostAmount << std::dec << std::endl;
+		}
+		else
+		{
+			std::cout << "Error when Reading Boost" << std::endl;
+		}
+		if (*Turtling == 3)
+		{
+			std::cout << "Turtling = True" << std::endl;
+			Worked = true;
+		}
+		if (*Turtling == 2)
+		{
+			std::cout << "Turtling = False" << std::endl;
+			Worked = true;
+		}
+		if (*Turtling == NULL || *Turtling != 2 && *Turtling != 3 && Worked == false)
+		{
+			std::cout << "Error when Reading Turtling." << std::endl;
+		}
 	}
 }
